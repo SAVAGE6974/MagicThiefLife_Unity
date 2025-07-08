c20be5f56d683893859d7afdd853d41d72accecf
SAVAGE6974 <jaeminan944@icloud.com>
2025-07-08 14:00:52 +0900

    Made MainMenuScene

---
+++ Assets/Scripts/Button/Button_GotoGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_GotoGame : MonoBehaviour
+{
+    public void OnclickGotoScene()
+    {
+        SceneManager.LoadScene("MainScene");
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void OnclickGotoLoadScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnclickQuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
--- Assets/Scripts/UI/PropertyManager.cs
+++ Assets/Scripts/UI/PropertyManager.cs
@@ -19,16 +19,16 @@ public class PropertyManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            PlayerPrefs.DeleteAll();
-            Debug.Log("All PlayerPrefs keys deleted");
-            currentMoney = 0; // 초기화
-        }
+        // if (Input.GetKeyDown(KeyCode.R))
+        // {
+        //     PlayerPrefs.DeleteAll();
+        //     Debug.Log("All PlayerPrefs keys deleted");
+        //     currentMoney = 0; // 초기화
+        // }
 
-        // 매 프레임 최신 돈을 가져와 UI에 표시
-        // PropertyManager가 돈을 변경하는 유일한 스크립트라면 이 줄은 필요 없을 수도 있지만,
-        // 다른 스크립트가 돈을 변경할 경우 즉시 반영하기 위해 유지하는 것이 좋습니다.
+        // // 매 프레임 최신 돈을 가져와 UI에 표시
+        // // PropertyManager가 돈을 변경하는 유일한 스크립트라면 이 줄은 필요 없을 수도 있지만,
+        // // 다른 스크립트가 돈을 변경할 경우 즉시 반영하기 위해 유지하는 것이 좋습니다.
         currentMoney = PlayerPrefs.GetInt("PlayerMoney", 0);
         PlayerMoneyTextUI.text = currentMoney + "$";
     }
